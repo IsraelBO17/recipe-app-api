fd7d70eb4af8a013cecd74e213dd7911a0d2c2ae
IsraelBO17 <israelboluwatife17@gmail.com>
2024-04-18 17:35:52 +0100

    Fixed Linting Errors.

--- app/core/models.py
+++ app/core/models.py
@@ -35,6 +35,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    
-    USERNAME_FIELD = "email"
 
+    USERNAME_FIELD = "email"
